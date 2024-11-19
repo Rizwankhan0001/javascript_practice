d849819cb080ccc8589ac1d6ff75777ac881755d
Rizwan Khan <113703223+Rizwankhan0001@users.noreply.github.com>
2024-11-19 08:26:43 +0000

    datatypes revision

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // it will treat all js code as a new version
+//alert (3+3) we are using it nodejs not browser
+
+console.log(3+3); //code readability should be high
+console.log("Rizwan Khan");
+
+let name="rizwan";
+let age=22;
+let isLoggedIn=false;
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+
+console.log(typeof undefined) ;  // =>undefined
+console.log(typeof null); // =>object
